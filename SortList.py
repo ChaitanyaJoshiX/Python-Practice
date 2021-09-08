5c0068729c7df7685e0d3a72f57372ac82905b35
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2021-09-08 20:26:56 +0530

    Add files via upload

---
+++ SortList.py
@@ -0,0 +1,21 @@
+"""
+Takes a integer list as input from user
+Sorts the list Using bubble sort method
+displays list before and after sorting
+"""
+list =[]
+print("````````````````````````````````````````````````````")
+print("Welcome to the list sorting program! Lets begin.")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+l = int(input("How many numbers will there be in your list? : "))
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Enter your numbers one by one.")
+for i in range(l):
+    temp = float(input("Enter number "+str(i+1)+" : "))
+    list.append(temp)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Original list :",list)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+list.sort()
+print("Sorted List :",list)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
