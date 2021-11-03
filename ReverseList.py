639083ab64b5f7fcfeefafc0dd40f14f51f14087
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2021-11-03 11:26:52 +0530

    Add files via upload

---
+++ ReverseList.py
@@ -0,0 +1,18 @@
+"""
+WAP to enter a list.
+Reverse the list and return the resulting list.
+Author : @ChaitanyaJoshiX
+"""
+inplist = input("Enter list elements separated by space : ")
+inplist = inplist.split()
+outlist = []
+l = len(inplist)
+j = l-1
+for i in range(l):
+    outlist.append(inplist[j])
+    j -= 1
+print(outlist)
+"""
+Later, I realised that the reverse() function is already present in a py lib.
+So, I remade the program in C++
+"""
