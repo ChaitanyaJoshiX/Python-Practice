03c58737563106fd8b4db086a8ab49f04376edaa
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2021-12-01 17:47:45 +0530

    Find the Area of a Circle

---
+++ AreaofCircle.py
@@ -0,0 +1,13 @@
+"""
+Starting of the program
+"""
+pi = 3.14 #Storing value of pi
+r = input("Enter the radius of circle : ")
+print(r) #Taking input from user
+r = float(r) #Converting string input to decimal
+area = pi * (r * r) #Storing the area of the circle
+print("Area of the circle = ", end = "")
+print(area) # Printing the area
+"""
+End of the program:)
+"""
