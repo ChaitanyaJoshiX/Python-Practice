ef9d42951d6c3b0ca7ae45790d316041489a8de6
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2021-12-03 09:50:30 +0530

    Add files via upload

---
+++ AtomicNumber.py
@@ -0,0 +1,21 @@
+"""
+Start of the program!
+"""
+ElementsList = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron"]
+AtomicList = [1, 2, 3, 4, 5]
+MassNumber = ["1", "4", "7", "9", "11"]
+Nature = ["Non-Metal", "Noble Gas", "Metal", "Metal", "Metalloid"]
+input = input("Enter the atomic number of the element : ")
+print(input)
+temp = input
+input = int(input)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+for i in range(5):
+    if input == AtomicList[i]:
+        print("Element\t  Atomic No.   Nature\tMass Number")
+        print(ElementsList[i]+"\t   "+temp+"\t       "+Nature[i] +"\t"+ MassNumber[i])
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Thank you for running my program :)")
+"""
+End of the program:)
+"""
